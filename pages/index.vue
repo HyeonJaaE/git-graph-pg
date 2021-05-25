8acb32598fd94178eaaf83234e335c2365c3c149
현재 이 <presentlee@nexon.co.kr>
2021-05-25 11:40:40 +0900

    12312

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,7 @@
       <Logo />
       <h1 class="title">patrick</h1>
       <h1 class="title">patrick</h1>
-      <h1 class="title">patrick</h1>
-      <h1 class="title">patrick</h1>
+
       <h1 class="title">patrick</h1>
       <h1 class="title">patrick</h1>
       <div class="links">
