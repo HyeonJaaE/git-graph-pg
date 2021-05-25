654cdfd84e00cdffc3ad8b3df07a2bcc4be527ae
현재 이 <presentlee@nexon.co.kr>
2021-05-25 11:47:40 +0900

    553535

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,12 @@
       <h1 class="title">patrick</h1>
       <h1 class="title">patrick</h1>
       <h1 class="title">patrick</h1>
+      <h1 class="title">patrick</h1>
+      <h1 class="title">patrick</h1>
+      <h1 class="title">patrick</h1>
+      <h1 class="title">patrick</h1>
+      <h1 class="title">patrick</h1>
+      <h1 class="title">patrick</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
